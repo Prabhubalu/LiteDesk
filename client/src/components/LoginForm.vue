2f954ba171a95c265f763ecb8de8466099d66005
Darshan <darshangem@gmail.com>
2025-10-25 21:52:14 +0530

    Updated RequestDemo Page
    Updated tailwind on request demo page & re-orderd form fields
    Added dialog after successful demo registration.

--- client/src/components/LoginForm.vue
+++ client/src/components/LoginForm.vue
@@ -22,18 +22,13 @@ const handleLogin = async () => {
 </script>
 
 <template>
-    <!-- <div class="welcome-section">
-        <h1 class="welcome-heading">Welcome back!</h1>
-        <p class="welcome-subtext">Sign in to your account</p>
-    </div> -->
-    <!-- <h2 class="mb-6 text-center text-2xl/9 font-bold tracking-tight text-gray-900 dark:text-white">Sign in to your account</h2> -->
     <form class="space-y-6" @submit.prevent="handleLogin">
         <div>
           <label for="email" class="block text-sm/6 font-medium text-gray-900 dark:text-white">Email address</label>
           <div class="mt-2">
             <input type="email" id="email" v-model="email" autocomplete="email" required placeholder="Email" 
-            class="block w-full rounded-md bg-gray-100 px-3 py-1.5 text-gray-900 outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6 
-            dark:text-white dark:bg-gray-700 dark:focus:bg-gray-800"/>
+            class="block w-full rounded-md bg-gray-100 px-3 py-1.5 text-gray-900 text-base outline-1 -outline-offset-1 outline-gray-300/20 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6 
+            dark:text-white dark:bg-gray-700 dark:focus:bg-gray-800 dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500"/>
           </div>
         </div>
 
@@ -46,13 +41,13 @@ const handleLogin = async () => {
           </div>
           <div class="mt-2">
             <input type="password" id="password" v-model="password" autocomplete="current-password" required placeholder="Password" 
-            class="block w-full rounded-md bg-gray-100 px-3 py-1.5 text-gray-900 outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6 
-            dark:text-white dark:bg-gray-700 dark:focus:bg-gray-800"/>
+            class="block w-full rounded-md bg-gray-100 px-3 py-1.5 text-gray-900 text-base outline-1 -outline-offset-1 outline-gray-300/20 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6 
+            dark:text-white dark:bg-gray-700 dark:focus:bg-gray-800 dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500"/>
           </div>
         </div>
 
         <div>
-          <button type="submit" :disabled="authStore.loading" class="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+          <button type="submit" :disabled="authStore.loading" class="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-2.5 text-md/0 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
             {{ authStore.loading ? 'Signing in...' : 'Sign In' }}
           </button>
           <p v-if="authStore.error" class="error">{{ authStore.error }}</p>
