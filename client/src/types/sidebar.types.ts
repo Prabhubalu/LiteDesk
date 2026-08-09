b922c3302b550230dc18c353de853b236abf75a1
PrabhuBalu <im.prabhub@gmail.com>
2026-08-09 16:13:03 +0530

    fixed error

--- client/src/types/sidebar.types.ts
+++ client/src/types/sidebar.types.ts
@@ -54,7 +54,7 @@ type SidebarLabelFields = {
 export type SidebarItem =
   | ({
       kind: 'surface';
-      id: 'home' | 'inbox' | 'live-chat' | 'announcements' | 'attention' | 'search' | 'trash' | 'approvals' | 'astra';
+      id: 'home' | 'inbox' | 'live-chat' | 'announcements' | 'telephony' | 'attention' | 'search' | 'trash' | 'approvals' | 'astra';
       route: string;
       icon?: string;
     } & SidebarLabelFields)
